9cf1d09950cf1f3c7e605080a6748ff3fa625f6d
Darko Vidic <darko.vidic2@gmail.com>
2024-07-07 21:49:51 +0200

    fix sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,7 +3,7 @@ import { MetadataRoute } from "next";
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: "https://www.class-master.com",
+      url: "https://class-master.com",
       lastModified: new Date(),
       changeFrequency: "yearly",
       priority: 1,
